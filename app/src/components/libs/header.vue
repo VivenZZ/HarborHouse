44664f25bb4930d1f8c76ad5f1664a905d28126f
VivenZZ <17706500512@163.com>
2018-09-13 00:28:17 +0800

    使用less 和新建heaerIndex组件

---
+++ app/src/components/libs/header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="header">
+    <p class="header_p" v-for="(item, index) in headerInfo" :key="index">
+      <span class="header_span">{{item.id}}</span>
+    </p>
+  </div>
+</template>
+<style lang="less" src='@/assets/less/libs/header.less' scoped></style>
+<script>
+export default{
+  name: 'HeaderIndex',
+  props: ['headerInfo']
+}
+</script>
---
+++ app/src/components/test.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="test">
+    <HeaderIndex :headerInfo='headerInfo' />
+  </div>
+</template>
+<script>
+import HeaderIndex from '@/components/libs/header'
+export default{
+  name: 'Test',
+  components: {
+    HeaderIndex
+  },
+  data () {
+    return {
+      headerInfo: [
+        {
+          id: 1
+        },
+        {
+          id: 2
+        },
+        {
+          id: 3
+        }
+      ]
+    }
+  }
+}
+</script>
